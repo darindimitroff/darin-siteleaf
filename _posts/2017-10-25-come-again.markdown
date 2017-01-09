21fe9b25d7d6f0accca6fa1f76def6f29efe253c
Darin Dimitroff <d@thecrazyones.agency>
2017-01-09 22:27:12 +0200

    WIP

---
+++ _posts/2017-10-25-come-again.markdown
@@ -0,0 +1,23 @@
+---
+layout: post
+title:  "Welcome to Jekyll!"
+date:   2017-01-09 09:11:03
+categories: jekyll update
+---
+
+Does this really work?
+
+Jekyll also offers powerful support for code snippets:
+
+{% highlight ruby %}
+def print_hi(name)
+  puts "Hi, #{name}"
+end
+print_hi('Tom')
+#=> prints 'Hi, Tom' to STDOUT.
+{% endhighlight %}
+
+Check out the [Jekyll docs][jekyll] for more info on how to get the most out of Jekyll. File all bugs/feature requests at [Jekyll's GitHub repo][jekyll-gh].
+
+[jekyll-gh]: https://github.com/mojombo/jekyll
+[jekyll]:    http://jekyllrb.com
--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
 gulp.task('sass', function () {
-    return gulp.src('_scss/main.scss')
+    return gulp.src('_scss/stylesheet.scss')
         .pipe(sass({
             includePaths: ['scss'],
             onError: browserSync.notify
@@ -56,8 +56,8 @@ gulp.task('sass', function () {
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('_scss/*.scss', ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+    gulp.watch('_scss/**/*.scss', ['sass']);
+    gulp.watch(['*.html', '_layouts/*.html', '_posts/*', '_includes/*.html'], ['jekyll-rebuild']);
 });
 
 /**
